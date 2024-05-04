0f04f19bfc85b2f73edfe52d2327cdf72999c344
sachin-learning-1 <sachin.learning.30@gmail.com>
2024-05-04 17:03:08 +0530

    Update Service Prinicipal variables for Kubernetes

---
+++ terraform/variables.tf
@@ -0,0 +1,10 @@
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+variable "client_id" {
+  description = "Azure Kubernetes Service Cluster service principal"
+}
+
+variable "client_secret" {
+  description = "Azure Kubernetes Service Cluster service secret"
+}
